caddcf31c743068503b39c024deb1204034aaca6
Bryce Lambert <brycelambert@gmail.com>
2013-09-24 18:15:20 -0400

    finished quiz

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -2,15 +2,16 @@
 puts "I am thinking of an integer between 0 and 10... what number am I thinking of? "
 
 # Obtain the user's input and interpret it as an integer.
-answer = 
+answer = gets.chomp.to_i
 
 # Generate a random number between 0 and 10
-secret = 
+secret = rand(11)
 
 # Use a while loop to let the user guess again until he/she gets the answer right.
-while 
-  
+while answer != secret
+  puts "Nope, sorry! Try again."
+  answer = gets.chomp.to_i
 end
 
 # Display a congratulatory response
-puts "that's it!  How'd you know?!"
+puts "Yes, it was #{secret}!  How'd you know?!"
