d9a8f0e21957cf8e594ebe14f5b192a6f2aa846c
Abby Howell <abby.g.howell@gmail.com>
2013-09-24 17:50:20 -0400

    fixed bug gets.chomp.to_i

--- guess_the_number_answer.rb
+++ guess_the_number_answer.rb
@@ -1,13 +1,12 @@
-
-
-
 puts "I am thinking of an integer between 0 and 10..."
 puts "What number am I thinking of? "
 answer = gets.chomp
 
 secret = rand(11)
+
 while answer != secret
 	puts "nope!  Try again!"
-	answer = gets.chomp
+	answer = gets.chomp.to_i
 end
+
 puts "That's it!  How'd you know?!"
