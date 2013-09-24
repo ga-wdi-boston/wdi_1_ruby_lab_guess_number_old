b235167a004f98d727c4a27d01d35da2a3d69459
Blake Ruddock <ruddock.blake@gmail.com>
2013-09-24 17:56:07 -0400

    Final commit

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -2,13 +2,17 @@
 puts "I am thinking of an integer between 0 and 10... what number am I thinking of? "
 
 # Obtain the user's input and interpret it as an integer.
-answer = 
+answer = gets.chomp
+answer = answer.to_i
 
 # Generate a random number between 0 and 10
-secret = 
+secret = rand(10)
 
 # Use a while loop to let the user guess again until he/she gets the answer right.
-while 
+while answer != secret
+	puts "That's not it!  Try another number..."
+	answer = gets.chomp
+	answer = answer.to_i
   
 end
 
