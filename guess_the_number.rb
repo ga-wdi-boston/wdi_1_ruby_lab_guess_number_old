b1d5da9989dc1800a8c10070541d9ee0454fec29
Rahul Horé <hore.rahul@gmail.com>
2013-09-24 21:25:27 -0400

    Refactors indefinite loop to one line

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,15 +1,12 @@
 # Display a prompt for the user
 print "I am thinking of an integer between 0 and 10... what number am I thinking of? "
 
-# Obtain the user's input and interpret it as an integer.
 # Generate a random number between 0 and 10
-answer, secret = gets.chomp.to_i, rand(11)
+secret = rand(11)
 
+# Obtain the user's input and interpret it as an integer.
 # Use a while loop to let the user guess again until he/she gets the answer right.
-until answer == secret
-  print "Nope! You're close, though. Try again. "
-  answer = gets.chomp.to_i
-end
+print "Nope! You're close, though. Try again. " until gets.chomp.to_i == secret
 
 # Display a congratulatory response
 puts "that's it!  How'd you know?!"
