608fd66924d750ea9e39466050b1303d57f9c2fa
Bobby Mazzotti <robert.mazzoti@gmail.com>
2013-10-05 16:17:42 -0400

    Finished quiz

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -3,6 +3,8 @@ puts "I am thinking of an integer between 0 and 10... what number am I thinking 
 
 # Obtain the user's input and interpret it as an integer.
 answer = gets.chomp.to_i
+f
+
 
 # Generate a random number between 0 and 10
 secret = rand(11)
@@ -11,6 +13,11 @@ secret = rand(11)
 while answer != secret
 	puts "Nope, try again. Don't gamble, you're not that good."
 	answer = gets.chomp.to_i
+
+# while answer >= 0 && answer >= 10 !=secret
+# 	puts "Nope, try again. Don't gamble, you're not that good."
+# 	answer = gets.chomp.to_i
+# if answer.is_a?
   
 end
 
