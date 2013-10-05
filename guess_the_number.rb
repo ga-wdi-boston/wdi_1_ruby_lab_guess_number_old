3fa22d16dddeac40b6c8546a82796c0aadc615d5
Amal Hussein <amalhussein08@gmail.com>
2013-10-05 15:01:13 -0400

    Change from Loop to While

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,16 +1,14 @@
 number = rand(1..10)
-num_guesses = 0
 
 puts "I'm thinking of a random number from 1 to 10. 
       Use your Jedi mind powers to guess the number."
-loop do
-  print "What is your guess? "
-  guess = gets.chomp.to_i
+guess = gets.chomp.to_i
 
-  unless guess == number
-    puts "Errr! Sorry. Wrong guess--try again Jedi. YOU CAN DO IT!"
-  else
-    puts "You got it!"
-    exit
-  end
+while guess != number
+	puts "Errr! Sorry. Wrong guess--guess again Jedi. YOU CAN DO IT!"
+	guess = gets.chomp.to_i
+end
+
+if guess == number
+	puts "You got it! Good job Jedi!"
 end
